33ec0fa08393b6530d51aa057a0db3f88287b48b
_SUBARU_3776_ <71259928+SUBARU3776@users.noreply.github.com>
2023-06-05 15:20:13 +0900

    Update Selenium.ver2.py

--- Selenium.ver2.py
+++ Selenium.ver2.py
@@ -7,22 +7,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# カレントディレクトリの変更
+# Change of current directory
 import os
-os.chdir(r'D:Selenium')  # ディレクトリを修正
+os.chdir(r'D:Selenium')  # Fix directory
 
 # Seleniumの設定
 options = Options()
-driver_path = r'D:\hogehoge\chromedriver.exe'  # Chrome Driverのパスを修正
+driver_path = r'D:\hogehoge\chromedriver.exe'  # Fix Chrome Driver paths
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service, options=options)
 
 try:
-    # ページを開く
+    # Open page
     url = 'https://twitter.com/i/flow/login'
     driver.get(url)
 
-    # ログイン情報を入力して次へをクリック
+    # Enter your login information
     username_input = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input'))
     )
